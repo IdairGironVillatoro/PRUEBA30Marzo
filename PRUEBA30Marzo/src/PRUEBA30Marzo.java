fd84da2444657ec0c9042ffca2ad10b1d8e4c3a4
usuario <usuario@192.168.0.106>
2020-04-02 15:45:31 -0600

    Primera Modificacion

---
+++ PRUEBA30Marzo/src/PRUEBA30Marzo.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class PRUEBA30Marzo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String Nombre, Direccion;
+        int telefono;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese nombre");
+            Nombre = teclado.nextLine();
+        System.out.println("Ingrese Direccion");
+            Direccion = teclado.nextLine();    
+        System.out.println("Ingrese Número Telefonico");
+            telefono = teclado.nextInt();    
+        System.out.println("Nombre..."+Nombre);
+        System.out.println("Dirección..."+Direccion);
+        System.out.println("Numero Telefonico..."+telefono);
+    }
+    
+}
